10625373fb446de1d7ccf677fbed2092a0d68b8d
vincebjaws <53380034+vincebjaws@users.noreply.github.com>
2021-08-31 14:52:01 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,17 @@
+What is complete:
+    -Remove
+    -Find
+    -Max Depth
+    -Size
+    -In Order Dump
+    -Level Order Dump
+    -Values At Level
+    -Has Root To Leaf Sum
+    -Are Identical
+    -Max Depth Iterative
+    -Iterative values at level
+
+What is incomplete:
+    -Intersection With:
+    		  The output is not right and I am nott sure how to fix
+		  this problem.
---
+++ TreeNode.hpp
@@ -0,0 +1,18 @@
+#ifndef _TreeNode
+#define _TreeNode
+class TreeNode
+{
+public:
+  TreeNode(): left(nullptr), right(nullptr), data(0) {}
+  TreeNode( int n ): left(nullptr), right(nullptr), data(n) {}
+
+  TreeNode *leftSubtree() { return left; }
+  TreeNode *rightSubtree() { return right; }
+  void leftSubtree( TreeNode *left ) { this->left = left; }
+  void rightSubtree(TreeNode *right) { this->right = right; }
+  int& value() { return data; }
+private:
+  TreeNode *left, *right;
+  int data;
+};
+#endif
